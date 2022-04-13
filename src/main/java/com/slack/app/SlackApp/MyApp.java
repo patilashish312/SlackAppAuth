2ab381ffb0c1a9fd57b6a9b2efa2eb5e7d66ed7a
unknown <ashishpatil_312@yahoo.com>
2022-04-13 12:53:43 +0100

    Initial commit

---
+++ src/main/java/com/slack/app/SlackApp/MyApp.java
@@ -0,0 +1,28 @@
+package com.slack.app.SlackApp;
+
+import java.io.IOException;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.slack.api.bolt.App;
+import com.slack.api.bolt.socket_mode.SocketModeApp;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@Slf4j
+public class MyApp {
+	public static void main(String[] args) throws IOException, Exception {
+		var app = new App();
+		app.command("/getvm", (req, ctx) -> {
+			var logger = ctx.logger;
+			try {
+				var result = ctx.client().chatPostMessage(
+						r -> r.token(ctx.getBotToken()).channel(ctx.getChannelId()).text(":wave: Hello"));
+			} catch (Exception e) {
+				logger.error("error: {}", e.getMessage(), e);
+			}
+			return ctx.ack();
+		});
+		new SocketModeApp(app).start();
+	}
+}
